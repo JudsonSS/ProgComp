87b3d8caecf16a8c15ff6430795bc476cf8d3edb
Judson Santiago <judson@ufersa.edu.br>
2021-04-30 03:54:10 -0300

    Semestre 2020.2

---
+++ Labs/Lab17/Apoio/Incremento.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 20;
+	int b = 20;
+
+	cout << "  a = " <<   a << ": b   = " << b << "\n";
+	cout << "++a = " << ++a << ": b++ = " << b++ << "\n";
+	cout << "  a = " <<   a << ": b   = " << b << "\n";
+
+	return 0;
+}
