ca082bf8dcdd03139a196e32f07be99fa0c9a03c
Henrique Chaves <81886464+henriquechav@users.noreply.github.com>
2025-01-21 14:46:40 -0300

    Corrige `lab12q3.cpp`
    A simplificação do problema impõe que cada mês tem 31 dias. Portanto, isso implica que um ano tem 31 * 12 = 372 dias.

--- Labs/Lab12/Respostas/lab12q3.cpp
+++ Labs/Lab12/Respostas/lab12q3.cpp
@@ -3,8 +3,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-// não foi usado "using namespace std;" porque o espaço de nomes std
-// já contém um elemento de nome "data", o que causaria um choque 
+// nÃ£o foi usado "using namespace std;" porque o espaÃ§o de nomes std
+// jÃ¡ contÃ©m um elemento de nome "data", o que causaria um choque 
 // com o tipo "data" definido no programa
 
 struct data
@@ -35,10 +35,10 @@ int main()
 int DiasDeVida(data nasc, data hoje)
 {
 	const int DiasPorMes = 31;
-	const int DiasPorAno = 365;
+	const int DiasPorAno = 31 * 12;
 	
-	// Na data 05/02/2010, temos 1 (um) mês completo e mais 5 dias em 2010
-	// Essa é a justificativa para subtrair 1 do valor do mês
+	// Na data 05/02/2010, temos 1 (um) mÃªs completo e mais 5 dias em 2010
+	// Essa Ã© a justificativa para subtrair 1 do valor do mÃªs
 	int ini = (nasc.ano * DiasPorAno) + ((nasc.mes - 1) * DiasPorMes) + nasc.dia;
 	int fim = (hoje.ano * DiasPorAno) + ((hoje.mes - 1) * DiasPorMes) + hoje.dia;
 
